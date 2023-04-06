ee17aa668e900aaa54182a6150d203557f8a6a01
Heber Duarte <heberduarteryr@gmail.com>
2023-04-05 23:20:15 -0300

    Hard Skill Repository, service

---
+++ src/main/java/com/portfolio/Repository/HardSkillRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.Repository;
+
+import com.portfolio.Entity.HardSkill;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HardSkillRepository extends JpaRepository<HardSkill, Integer> {
+
+    public List<HardSkill> findAllByUsersUsername(String username);
+}
---
+++ src/main/java/com/portfolio/Service/HardSkillService.java
@@ -0,0 +1,33 @@
+package com.portfolio.Service;
+
+import com.portfolio.Entity.HardSkill;
+import com.portfolio.Repository.HardSkillRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class HardSkillService {
+
+    @Autowired
+    HardSkillRepository hsR;
+
+    public List<HardSkill> getByUser(String username) {
+        return hsR.findAllByUsersUsername(username);
+    }
+
+    public void save(HardSkill hs) {
+        hsR.save(hs);
+    }
+
+    public void delete(int id) {
+        hsR.deleteById(id);
+    }
+
+    public boolean existsById(int id) {
+        return hsR.existsById(id);
+    }
+
+}
