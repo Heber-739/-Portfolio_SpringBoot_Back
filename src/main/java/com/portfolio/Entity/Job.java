561d2f512842cb1c4053110cbe1c3ba3565da4b2
Heber Duarte <heberduarteryr@gmail.com>
2023-04-06 14:19:04 -0300

    Job Entity

---
+++ src/main/java/com/portfolio/Entity/Job.java
@@ -0,0 +1,73 @@
+package com.portfolio.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "job")
+public class Job implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(150)")
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "user_username", referencedColumnName = "username", columnDefinition = "VARCHAR(16)")
+    private User user;
+
+    public Job() {
+    }
+
+    public Job(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
